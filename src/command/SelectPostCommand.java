7ffb84c27768837971c605970843a12cb634e31b
kwangho-park <44250982+kwangho-park@users.noreply.github.com>
2019-04-03 23:19:59 +0900

    Add files via upload

--- src/command/SelectPostCommand.java
+++ src/command/SelectPostCommand.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.PostInfoDao;
 import dto.PostInfoDto;
+import service.PostPaging;
 
 public class SelectPostCommand implements Command{
   
@@ -15,12 +16,20 @@ public class SelectPostCommand implements Command{
 		
 		PostInfoDao postInfoDao = new PostInfoDao();
 		
+		PostPaging postPaging = new PostPaging();
+		
+		
 		ArrayList<PostInfoDto> listDto = postInfoDao.selectAllPost();
 	
+		
+		int totalCount = postInfoDao.selectTotalCount();
+		
+		int[] countPage = postPaging.countPage(totalCount);
+		
 	
 		// recommend book page 로 전달하기위한 셋팅
 		request.setAttribute("listDto", listDto);
-		
+		request.setAttribute("countPage", countPage);
 		
 	} // execute() END
 	
