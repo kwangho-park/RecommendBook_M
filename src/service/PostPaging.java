4939e3ad64f9fd5487f5d372a94ecf4b888bb455
kwangho-park <44250982+kwangho-park@users.noreply.github.com>
2019-04-06 19:37:57 +0900

    Add files via upload

--- src/service/PostPaging.java
+++ src/service/PostPaging.java
@@ -5,9 +5,8 @@ public class PostPaging {
 	public PostPaging() {}
 	
 
-	// web page에 출력하는 page 번호 // 
-	// One web page = Ten post
-	public int[] countPage(int totalCount) {
+	// countPage를 셋팅하기 위한 resource : start/end page // 
+	public int[] countPage(int pageNum, int totalCount) {
 		
 		int[] resultArr = new int[4];
 		
@@ -15,12 +14,13 @@ public class PostPaging {
 		int startPage = 0;
 		int endPage = 0;
 		int totalPage = 0;				// 전체 페이지 수 (=페이지 번호) 
-		int countList = 10; 			// 한 페이지에 출력될 게시글 수 (max 10)
-		int countPage = 10;				// 한 페이지에 출력될 page 번호 (max 10)
+		int countList = 10; 			// 한 페이지에 출력될 게시글 수
+		int countPage = 10;				// 한 페이지에 출력될 page 번호
 		
-		// test
-		int page = 1;					// 현재 page
-										// 추후 client에서 parameter로 전달받아야함
+		int pageGroup = 0;				// countPage의 그룹 (countPage * 10(max) = 1group)
+		
+		final int limitList = 10;
+		final int limitPage = 10;				
 		
 		
 		// 전체 페이지 수를 구하는 logic //
@@ -33,8 +33,27 @@ public class PostPaging {
 		
 		
 		// start/end page logic //  
-		startPage = ((page-1)/10) * 10 +1;
+		/*
+		startPage = ((pageNum-1)/10) * 10 +1;
 		endPage = startPage + countPage - 1;
+		*/
+		
+		if(pageNum<=10) {
+			startPage = 1;
+			endPage = 10;
+			
+		}else if((pageNum>10) && (pageNum<=20)){
+			startPage = 11;
+			endPage = 20;
+			
+		}else if((pageNum>20) && (pageNum<=30)) {
+			startPage = 21;
+			endPage = 30;			// 추후 totalPage로 변경예정
+		}
+		
+		pageGroup = (totalPage / 10) + 1;
+		
+
 		
 		// 마지막 페이지 수 보정
 		if(endPage > totalPage) {
